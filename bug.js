05865e3359cca53ed1d44aa38637f44065dbcfd9
BugHunter-X <askzenai@gmail.com>
2025-02-22 23:38:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+// Example showing the error
+<div class="bg-gray-100 p-4 rounded-lg">
+  <h2 class="text-xl font-bold mb-2">Uncommon Tailwind CSS Bug</h2>
+  <p class="text-gray-700">This is some text that should be styled correctly.</p>
+  <p class="text-gray-700 text-base">This is more text with some specific styles</p>
+</div>
+```
